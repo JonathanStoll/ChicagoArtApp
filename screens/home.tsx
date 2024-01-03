ec2da6c3c35a498d7e0093969aa5fcb9f692615a
JonathanStoll <jonafstoll@gmail.com>
2024-01-03 14:09:50 -0300

    first data

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import axios from 'axios';
+import ArtList from '../components/artList';
 
-const Home: React.FC = () => {
+type MyComponentProps = {
+  navigation: any;
+};
+
+const Home: React.FC<MyComponentProps> = ({navigation}) => {
+  interface ArtWork {
+    title: string;
+  }
+
+  const [artWorks, setArtWorks] = useState<ArtWork[]>([]);
+
+  useEffect(() => {
+    interface NewElement {
+      title: string;
+    }
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response = await axios.get(
+          'https://api.artic.edu/api/v1/artworks',
+        );
+        const newArray: NewElement[] = response.data.data.map(
+          (element: any) => ({
+            title: element.title,
+          }),
+        );
+        setArtWorks(newArray);
+        console.log(response.data.data);
+        console.log(newArray);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
+      {artWorks.length > 0 && <ArtList artWorks={artWorks} />}
+
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button title="Go to fsvs" onPress={() => navigation.navigate('Favs')} />
     </View>
   );
 };
